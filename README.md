0af58980b109a30f87fbeb598c02fef362a1fa3e
Jvniorrr <49306278+jvniorrr@users.noreply.github.com>
2020-07-26 19:46:03 -0700

    Initialized repo with code & Updated ReadMe steps
    Initialized repo with code and to-do's for this module. Working except CC it seems.

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# off-white-master
- Automation for Off white c/o Virgil Abloh
+# OFF-WHITE
+- Off white script, autocheckout tool. 
+* Has support for Paypal Checkout currently & multiple profiles. 
+* Test for Off white Jordan 4
+
+## Table of Contents
+* [Installation](#Installation)
+* [Setup](#Setup)
+* [To Do](#TO-DO-List)
+
+
+# INSTALLATION
+1. Install Python 3: <https://www.python.org/downloads/windows/> or <https://www.python.org/downloads/mac-osx/>
+2. Open a CMD Prmpt window or a Terminal Window and run `python3` to make sure it's installed. If it runs fine and shows Python 3.x.x Then just put in cmd `quit()` to exit. Else look up error google it.
+3. Once Python 3 is installed. change your directory in the cmd prmpt or terminal to where the unzipped file is. 
+Win: `C:\Users\userName\Downloads\off-white` or `C:<Path>`
+Mac: `cd /Users/userName/downloads/off-white` or `cd <path>`
+4. When in that directory, try to run `pip3 install -r requirements.txt` if it doesn't work run ``pip install -r requirements.txt`
+5. If it installs all correctly, will take a while probably. Then run `python3 main.py`
+
+# Setup
+* After having done Installation Steps follow the steps here. 
+* Setting up Profiles
+```json 
+    {
+    "profiles":[
+        {
+    "profile":"Profile Name",
+    "firstName":"First",
+    "lastName":"Last",
+    "phone":"1234567890",
+    "email": "email@email.com",
+    "addressLine1":"123 main str",
+    "addressLine2":"",
+    "addressLine3":"",
+    "city":"los angeles",
+    "state":"California",
+    "country":"US",
+    "zipCode":"90012",
+    "card": {
+        "name":"cardholder Name",
+        "number": "4242424242424242",
+        "expyear":2023,
+        "expmonth": 6,
+        "cvv":"123"
+    },
+    "proxies":null, 
+    "paypal":false,
+    "total":1
+        }
+    ],
+    "webhook":"DiscordWebhook",
+    "captcha": "APIkey"
+}
+```
+Proxies value should be `null` to use local host, or a direct path of the proxies.txt file to use. Paypal should be `true` or `false`. Webhook should be a string (surrounded in quotes) and itll send successfull webhook / failure. Captcha is your 2 captcha API key. just pass in there as a string.
+* "proxies": "C:\Users\userName\Downloads\off-white\proxies.txt"
+* "paypal" : true
+* "total": Integer Value with total tasks for that profile
+* "webhook": "discordWebhook" 
+* "captcha": "2captcha API key" 
+* To add another profile, just copy the default one, and add 1 comma at the end (after "}") of the dictionary / profile you had just copied. Would end up being `},`
+
+
+## TO-DO List
+| **Done** | **News** |
+| -------- | -------- |
+| ✅| Add retries to request if any errors arise  |
+| ✅| Find a way to add better handling specifically for site crashes (Fixed think)  |
+| ✅| Add threading for multiple profile use |
+| ✅| Add a simple terminal based UI / menu |
+| ✅| Add support for proxy groups  |
+| ✅| Add support for localhost |
+| ❌| Add time outs to requests so sessions wont be hanging forever |
+| ❌| Add multi-processing / or asynchronous code  |
+| ❌| Add proxy handling: dead proxies |
+| ❌| Fix CC support ASAP!!! |
---
+++ main.py
@@ -0,0 +1,92 @@
+from consolemenu.items import *
+from consolemenu import *
+from consolemenu.format import *
+from consolemenu.menu_component import Dimension
+import os, time, subprocess, platform, json, threading
+from src.botFinalDraft import Bot
+from cfonts import render, say
+
+
+
+def termUI():
+    # grab dir of file
+    wd = os.path.dirname(os.path.realpath(__file__))
+
+
+
+    # set the formatt of the Term UI
+    thin = Dimension(width=79, height=40)
+    menu_format = MenuFormatBuilder(max_dimension=thin)
+    menu_format.set_prompt(">")
+    menu_format.set_title_align('center')                   # Center the menu title (by default it's left-aligned)
+    menu_format.set_subtitle_align('center')
+    menu_format.set_prologue_text_align('center')           # Center the prologue text (by default it's left-aligned)
+    menu_format.show_prologue_bottom_border(True)           # Show a border under the prologue
+    menu_format.set_border_style_type(MenuBorderStyleType.DOUBLE_LINE_OUTER_LIGHT_INNER_BORDER)
+    menu_format.set_items_top_padding(3)
+
+
+    # create the menu w/ selection class
+    a_list=["Start Bot", "Profiles & Task Config", "Proxies"] # the options available
+    mainMenu = SelectionMenu(
+        a_list, exit_option_text='Exit :(', formatter=menu_format, title='Off---White', subtitle='Jvnior AIO / MODUS IO'
+        )
+
+    mainMenu.show()
+    mainMenu.join() # return the recent response from this
+
+    menu = mainMenu.selected_option
+
+    if mainMenu.selected_option == 0:
+        # User Started Bot
+        mainMenu.screen.clear()
+        link = mainMenu.screen.input('Link: ')
+        mainMenu.screen.clear()
+
+        config_file = os.path.join(wd,'config.json')
+        json_file = open(config_file, 'r', encoding='utf-8')
+        info = json.load(json_file)
+        json_file.close()
+        hook = info['webhook']
+        profiles = info['profiles']
+        captcha = info['captcha']
+        threads = list()
+
+        # run the actual tasks for every profile in profiles
+        output = render('Off White', colors=['red'], align='left', font='pallet')
+        output2 = render('✨ c/o Jvnior OW ✨', colors=['candy'], align='left', font='console')
+        print(output, output2)
+        for profile in profiles:
+            t = threading.Thread(target=Bot(url=link, info=profile, hook=hook, captcha=captcha).tasks, args=())
+            t.start()
+            threads.append(t)
+        for thread in threads:
+            thread.join()
+
+
+
+    elif mainMenu.selected_option == 1:
+        # User Viewing Task Configuration
+        mainMenu.screen.clear()
+        filepath = os.path.join(wd, 'config.json')
+        if platform.system() == 'Darwin':       # macOS
+            file = subprocess.call(('open', filepath))
+        elif platform.system() == 'Windows':    # Windows
+            file = os.startfile(filepath)
+
+    elif mainMenu.selected_option == 2:
+        # User Viewing Proxies
+        mainMenu.screen.clear()
+        filepath = os.path.join(wd, 'proxies.txt')
+        if platform.system() == 'Darwin':       # macOS
+            file = subprocess.call(('open', filepath))
+        elif platform.system() == 'Windows':    # Windows
+            file = os.startfile(filepath)
+
+    elif mainMenu.selected_option == 3:
+        # User Exited Program
+        print("Exited bot")
+
+
+if __name__ == "__main__":
+    termUI()
